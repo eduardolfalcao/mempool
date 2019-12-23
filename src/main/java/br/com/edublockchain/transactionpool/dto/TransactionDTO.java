6474f0766a8eb7932cdccafb1339442e1f2e4b41
Eduardo Falcão <eduardolfalcao@gmail.com>
2019-12-23 11:25:49 -0300

    hashCode and equals on DTO adjusted for uniqueID

--- src/main/java/br/com/edublockchain/transactionpool/dto/TransactionDTO.java
+++ src/main/java/br/com/edublockchain/transactionpool/dto/TransactionDTO.java
@@ -4,18 +4,58 @@ import java.util.Date;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 @Data
 @AllArgsConstructor
 @Getter
-@EqualsAndHashCode
 public class TransactionDTO {
 	
 	private String sender;
 	private String receiver;
 	private double amount, fee;
 	private Date creationTime;
+	private String uniqueID;
+	
+	@Override
+	public int hashCode() {
+		return uniqueID.hashCode();
+	}	
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		TransactionDTO other = (TransactionDTO) obj;
+		if (Double.doubleToLongBits(amount) != Double.doubleToLongBits(other.amount))
+			return false;
+		if (creationTime == null) {
+			if (other.creationTime != null)
+				return false;
+		} else if (!creationTime.equals(other.creationTime))
+			return false;
+		if (Double.doubleToLongBits(fee) != Double.doubleToLongBits(other.fee))
+			return false;
+		if (receiver == null) {
+			if (other.receiver != null)
+				return false;
+		} else if (!receiver.equals(other.receiver))
+			return false;
+		if (sender == null) {
+			if (other.sender != null)
+				return false;
+		} else if (!sender.equals(other.sender))
+			return false;
+		if (uniqueID == null) {
+			if (other.uniqueID != null)
+				return false;
+		} else if (!uniqueID.equals(other.uniqueID))
+			return false;
+		return true;
+	}
 	
 }
